c1b5aa382bfd21a94fcbc65470acf5054caa9dca
rxhuljoshi10 <125109396+rxhuljoshi10@users.noreply.github.com>
2024-01-05 17:37:13 +0530

    Add files via upload

---
+++ Jarvis/Modules.py
@@ -0,0 +1,27 @@
+
+from gtts import gTTS
+import pyttsx3
+import pygame
+import json
+import os
+import re
+import time
+import clipboard
+import random
+import pyautogui as p
+import wikipedia
+import webbrowser
+import requests
+import pyjokes
+import threading
+from queue import Queue
+# import openpyxl
+import psutil
+# from PyDictionary import PyDictionary
+from datetime import datetime
+from pywikihow import search_wikihow
+from bs4 import BeautifulSoup
+from deep_translator import GoogleTranslator
+from googletrans import LANGCODES,LANGUAGES
+import speech_recognition as sr
+import keyboard
